8091d518bbeffe16721a76182c2e60eaf687b02f
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2022-07-01 22:36:05 -0700

    Add discord mention role

--- format_release_notes.py
+++ format_release_notes.py
@@ -1,5 +1,6 @@
 import sys
 
+print("Hey <@&{0}>, we've got a new release!".format(sys.argv[3]))
 print("**Release Notes v{0}**".format(sys.argv[2]))
 print("**What's New**")
 with open(sys.argv[1], "r") as f:
